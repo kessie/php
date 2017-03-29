8187df79472102bfeab4e167286c82ab8e957e0c
kessie <kessiena.o@gmail.com>
2017-03-29 17:50:10 +0100

    show.php
    contains .php code which is called in the phpAssignment.php code as shown below
    <form action="show.php" method="post">
    </form>
    it basically tells the form what to do when "submit" is clicked
    P.S: work

---
+++ show.php
@@ -0,0 +1,4 @@
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+Your age is: <?php echo $_POST["age"]; ?><br>
+Your city is: <?php echo $_POST["city"]; ?><br>
